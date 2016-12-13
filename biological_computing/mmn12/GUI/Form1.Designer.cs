cbe5ece30b41a7e89196f1958062de4e213217a4
dovh <lilach16@gmail.com>
2016-12-14 00:35:44 +0200

    good night

--- biological_computing/mmn12/GUI/Form1.Designer.cs
+++ biological_computing/mmn12/GUI/Form1.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.ChartControl = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.ExitButton = new System.Windows.Forms.Button();
             this.RunButton = new System.Windows.Forms.Button();
@@ -43,33 +43,37 @@
             this.PauseButton = new System.Windows.Forms.Button();
             this.InitializeButton = new System.Windows.Forms.Button();
             this.StepButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SelectionThresholdTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.ChartControl)).BeginInit();
             this.SuspendLayout();
             // 
             // ChartControl
             // 
-            chartArea1.Name = "ChartArea1";
-            this.ChartControl.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.ChartControl.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.ChartControl.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.ChartControl.Legends.Add(legend2);
             this.ChartControl.Location = new System.Drawing.Point(12, 12);
             this.ChartControl.Name = "ChartControl";
-            series1.BorderWidth = 2;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Color = System.Drawing.Color.Red;
-            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            series1.Legend = "Legend1";
-            series1.Name = "Average";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Max";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Min";
-            this.ChartControl.Series.Add(series1);
-            this.ChartControl.Series.Add(series2);
-            this.ChartControl.Series.Add(series3);
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.Red;
+            series4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series4.Legend = "Legend1";
+            series4.Name = "Average";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Max";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Min";
+            this.ChartControl.Series.Add(series4);
+            this.ChartControl.Series.Add(series5);
+            this.ChartControl.Series.Add(series6);
             this.ChartControl.Size = new System.Drawing.Size(571, 405);
             this.ChartControl.TabIndex = 0;
             this.ChartControl.Text = "chart1";
@@ -156,11 +160,47 @@
             this.StepButton.UseVisualStyleBackColor = true;
             this.StepButton.Click += new System.EventHandler(this.StepButton_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(618, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Selection filter (%):";
+            // 
+            // SelectionThresholdTextBox
+            // 
+            this.SelectionThresholdTextBox.Location = new System.Drawing.Point(738, 90);
+            this.SelectionThresholdTextBox.Name = "SelectionThresholdTextBox";
+            this.SelectionThresholdTextBox.Size = new System.Drawing.Size(72, 20);
+            this.SelectionThresholdTextBox.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(618, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "SteadyState threshold:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(738, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(72, 20);
+            this.textBox1.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(837, 430);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SelectionThresholdTextBox);
             this.Controls.Add(this.StepButton);
             this.Controls.Add(this.InitializeButton);
             this.Controls.Add(this.PauseButton);
@@ -192,6 +232,10 @@
         private System.Windows.Forms.Button PauseButton;
         private System.Windows.Forms.Button InitializeButton;
         private System.Windows.Forms.Button StepButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox SelectionThresholdTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
