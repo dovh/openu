7bdd096fdbccbda6abd94d515c6c7ee32dcd5d6b
dovh <lilach16@gmail.com>
2017-01-07 23:10:31 +0200

    .

--- biological_computing/mmn13/Project/Source/Neuron.cs
+++ biological_computing/mmn13/Project/Source/Neuron.cs
@@ -9,54 +9,52 @@ namespace WindowsFormsApplication1.Source
 {
     class Neuron
     {
-        int m_x;
-        int m_y;
-        double m_value; 
+        int m_X;
+        int m_Y;
+        double m_Value; 
 
-        Dictionary<Neuron, double> m_inputs;
+        Dictionary<Neuron, double> m_Inputs;
 
-        public int X { get { return m_x; } }
-        public int Y { get { return m_y; } }
-        public double Value { get { return m_value; } }
+        public int X { get { return m_X; } }
+        public int Y { get { return m_Y; } }
+        public double Value { get { return m_Value; } }
 
         public Neuron(int x, int y)
         {
-            m_x = x;
-            m_y = y;
-            m_inputs = new Dictionary<Neuron, double>();
+            m_X = x;
+            m_Y = y;
+            m_Inputs = new Dictionary<Neuron, double>();
         }
 
         public void ConnectInput(Neuron N)
         {
             Debug.Assert(X != N.X || Y != N.Y);
-            Debug.Assert(!m_inputs.ContainsKey(N));
+            Debug.Assert(!m_Inputs.ContainsKey(N));
 
-            m_inputs[N] = 0;
+            m_Inputs[N] = 0;
         }
 
         public void UpdateWeight(Neuron Ni, double Weight)
         {
             Debug.Assert(X != Ni.X || Y != Ni.Y);
-            Debug.Assert(m_inputs.ContainsKey(Ni));
-            m_inputs[Ni] = Weight;
+            Debug.Assert(m_Inputs.ContainsKey(Ni));
+            m_Inputs[Ni] = Weight;
         }
 
         public void Calculate()
         {
-            double value = 0;
-            foreach (KeyValuePair<Neuron, double> input in m_inputs)
+            m_Value = 0;
+            foreach (KeyValuePair<Neuron, double> input in m_Inputs)
             {
                 Neuron Ni = input.Key;
-                double weight = Ni.Value;
-                value += Ni.Value * weight;
+                double Weight = input.Value;
+                m_Value += Ni.Value * Weight;
             }
-
-            m_value = value;
         }
 
         public void Randomize()
         {
-            m_value = RandomGen.Get;
+            m_Value = RandomGen.Get;
         }
 
     }
